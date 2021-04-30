e20fcedb880e87c227fb9bdf79f8eb338a232d42
Matheus Noronha <matheus-nsantos@live.com>
2021-04-29 22:18:28 -0300

    Implementando data e Hora

--- src/Components/letreiro/banner.js
+++ src/Components/letreiro/banner.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './banner.css';
 
+
+
+
+const zeroFill = n => {
+  return ('0' + n).slice(-2);
+}
+
+const interval = setInterval(() => {
+  // Pega o horário atual
+  const now = new Date();
+
+  // Formata a data conforme dd/mm/aaaa hh:ii:ss
+  const dataHora = zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+
+  // Exibe na tela usando a div#data-hora
+  document.getElementById('ban2').innerHTML = dataHora;
+}, 1000);
+
 const Banner1 = () => {
-  const data = new Date();
+  // const data = new Date();
   return ( 
     <>
         <div className="banner1">
             <p>
-              12
+             Lopés
+            </p>
+            <p>
+              <p id="ban2"></p>
             </p>
             <p>
                 Estamos aqui por você confira nossas novidades!!
